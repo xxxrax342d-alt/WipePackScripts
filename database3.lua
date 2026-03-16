3dd63971fd52e8362d0be9cede09ae3eb79fe26a
xxxrax342d-alt <xxxrax342d@gmail.com>
2026-03-16 09:09:45 +0300

    Update database3.lua

--- database3.lua
+++ database3.lua
@@ -1,4 +1,3 @@
-```lua
 local ACCOUNT_ID = 3
 
 local Players = game:GetService("Players")
@@ -165,4 +164,3 @@ require(ReplicatedStorage.Events).ClientListen("PlayerAbilityEvent", function(da
 end)
 
 updateCounterDisplay()
-```
